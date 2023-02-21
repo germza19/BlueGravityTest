52ff58421c0c45af4ff4cd82212bfb07ec59ac44
German <germza123@gmail.com>
2023-02-21 15:33:01 -0300

    Documentation & Final polish. Thanks!

--- BlueGravityProject/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ BlueGravityProject/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private SoundManager soundManager;
+    [SerializeField] private GameObject mainButtonsGO;
+    [SerializeField] private GameObject optionsPanelGO;
 
     private void Awake()
     {
         soundManager.StartMenuMusic();
+        ActivatePanel(mainButtonsGO, true);
+        ActivatePanel(optionsPanelGO, false);
     }
     public void StartGame()
     {
@@ -18,5 +22,19 @@ public class MainMenuManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void OpenOptionsPanel()
+    {
+        ActivatePanel(mainButtonsGO, false);
+        ActivatePanel(optionsPanelGO, true);
+    }
+    public void CloseOptionsPanel()
+    {
+        ActivatePanel(mainButtonsGO, true);
+        ActivatePanel(optionsPanelGO, false);
+    }
+    public void ActivatePanel(GameObject go, bool value)
+    {
+        go.SetActive(value);
     }
 }
